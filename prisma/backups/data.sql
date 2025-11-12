98acf9b6ae87b18668a50d6698d71994ee5315a6
mjnexgen <mjnexgen@users.noreply.github.com>
2025-11-12 18:58:30 +0000

    Supabase backup

--- prisma/backups/data.sql
+++ prisma/backups/data.sql
@@ -4,7 +4,7 @@ SET session_replication_role = replica;
 -- PostgreSQL database dump
 --
 
--- \restrict l1ENTCgKwbuCVXal2766BRb0z89LcmU4AHICt92QThd3DOHx9Te1CHMUrKWICM7
+-- \restrict iUO8lhNFjCCNOZLppHJZNGqRoq18bfKuzgrcV2sdonpv13s6jAe3Y8W6SzGXYm0
 
 -- Dumped from database version 17.4
 -- Dumped by pg_dump version 17.6
@@ -8743,6 +8743,6 @@ SELECT pg_catalog.setval('"supabase_functions"."hooks_id_seq"', 103, true);
 -- PostgreSQL database dump complete
 --
 
--- \unrestrict l1ENTCgKwbuCVXal2766BRb0z89LcmU4AHICt92QThd3DOHx9Te1CHMUrKWICM7
+-- \unrestrict iUO8lhNFjCCNOZLppHJZNGqRoq18bfKuzgrcV2sdonpv13s6jAe3Y8W6SzGXYm0
 
 RESET ALL;
